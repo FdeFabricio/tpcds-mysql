04577c5b97efa524476982a23cef12a02c0fd757
Fabricio Ferreira <fabricio383@gmail.com>
2019-11-03 16:39:35 +0100

    adds script to run the pipeline

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# install mysql 8.0
+cd /tmp && \
+curl -OL https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb && \
+sudo dpkg -i mysql-apt-config* && \
+sudo apt-get update && \
+sudo apt-get -y install mysql-server -y
+
+# setup logging
+sudo chmod -R 777 /var/log/mysql
+sudo mysql -uroot -e "SET GLOBAL local_infile = 1;"
+sudo mysql -uroot -e "SET GLOBAL slow_query_log_file ='/var/log/mysql/slow-query.log';"
+sudo mysql -uroot -e "SET GLOBAL slow_query_log = 1;"
+sudo mysql -uroot -e "SET GLOBAL long_query_time = 0;"
+sudo mysql -uroot -e "SET long_query_time = 0;"
+
+# add source code to home directory 
+cd ~ && git clone https://github.com/FdeFabricio/tpcds-mysql.git
